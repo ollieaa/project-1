84c36178631ea6e1ca4e39ad59f1d82234b0d1a2
Ollie Anderson <ollieaa17@gmail.com>
2021-01-16 21:38:36 +0000

    enemy and player movement

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//TEST BUTTONS
+const move = document.querySelector('#move')
+
 //GLOBAL VARIABLES
 const cells = []
 const width = 10
 let lives = 3 
 let score = 0
 const grid = document.querySelector('#grid')
+let playerPos = 5
 
 //Generate an array of arrays of dom objects, making up the cells of the grid. 
 //Having a seperate array for each row in the grid will make life easier down the line.
@@ -22,7 +26,8 @@ for (let column = 0; column < width; column++) {
     
   }
 }
-// Set the cells in the odd numbered rows to have a class of left, and the even numbered rows to have class of right
+// Set the cells in the odd numbered rows to have a class of left, and the even numbered 
+//rows to have class of right, so that enemy cells know which direction to move
 for (let y = 9; y >= 0; y--) {
   for (let x = 9; x >= 0; x--) {
     if ((y % 2) === 1) {       
@@ -33,16 +38,98 @@ for (let y = 9; y >= 0; y--) {
   }
 }
 
-//Function to populate the grid with the starting enemies
+//EVENT LISTENERS
+//player movement
+document.addEventListener('keydown', (event) => {
+  const key = event.key
+  if (key === 'ArrowLeft' && !(playerPos === 0)) {
+    changeCell(cells[9][playerPos], 'player', 'empty')
+    changeCell(cells[9][playerPos-1], 'empty', 'player')
+    playerPos--
+  } else if (key === 'ArrowRight' && !(playerPos === 9)) {
+    changeCell(cells[9][playerPos], 'player', 'empty')
+    changeCell(cells[9][playerPos+1], 'empty', 'player')
+    playerPos++
+  }
+})
+//player shoot
+document.addEventListener('keydown', (event) => {
+  const key = event.key
+  if (key === ' ') {
+    if (cells[8][playerPos].classList.contains('enemy')) {
+      changeCell(cells[8][playerPos], 'enemy', 'empty')
+      score++
+    } else {
+      changeCell(cells[8][playerPos], 'empty', 'bullet')
+      for (let y = 8; y >=0; y--) {
+        for (let x = 0; x <=9)
+      }
+    }
+  }
+})
+
+//FUNCTIONS
+//Change cell class
+function changeCell(cellNum, remove, add) {
+  cellNum.classList.remove(`${remove}`)
+  cellNum.classList.add(`${add}`)
+}
+
+
+
+//Populate the grid with the starting enemies
 function startingEnemies() {
 
-  cells[9][6].classList.remove('empty')
-  cells[9][5].classList.add('player')
+  changeCell(cells[9][5], 'empty', 'player')
 
   for (let y = 3; y >= 0; y-- ) {
     for (let x = 7; x > 1; x--){
-      cells[y][x].classList.remove('empty')
-      cells[y][x].classList.add('enemy')     
+      changeCell(cells[y][x], 'empty', 'enemy')    
     }
   }
-}  
+} 
+
+//Move enemies. To be used within an interval
+function moveEnemies()  {
+  for (let y = 8; y >= 0; y--) {
+    //FOR LEFT CELLS
+    if (y % 2 === 1) {
+      for (let x = 0; x <= 9; x++) {
+        let thisCell = cells[y][x]
+        if (cells[y][x].classList.contains('enemy')) {
+          changeCell(thisCell, 'enemy', 'empty')
+          if (x === 0) {
+            changeCell(cells[y+1][x], 'empty', 'enemy')
+          } else {
+            changeCell(cells[y][x-1], 'empty', 'enemy')
+          }
+        }
+      }
+    //FOR RIGHT CELLS  
+    } else {
+      for (let x = 9; x >= 0; x--) {
+        let thisCell = cells[y][x]
+        if (thisCell.classList.contains('enemy')) {
+          changeCell(thisCell, 'enemy', 'empty')
+          if (x === 9 && y === 8) {
+            lives-=1
+          } else if (x === 9) {
+            changeCell(cells[y+1][x], 'empty', 'enemy')
+          } else {
+            changeCell(cells[y][x+1], 'empty', 'enemy')
+          }
+        }
+      }
+    }   
+  }
+} 
+
+//Player Shooting
+
+
+//TESTING FUNCTIONS
+startingEnemies()
+
+move.addEventListener('click', () => {
+  moveEnemies()
+})
