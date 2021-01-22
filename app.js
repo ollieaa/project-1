63eae51189cd4802968e78ae0bf6ed6b21f22528
Ollie Anderson <ollieaa17@gmail.com>
2021-01-22 12:50:09 +0000

    audio balance

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ vamp.loop
 vamp.volume = 0.7
 const gunshot = new Audio('./audio/gunshot.wav')
 const ouch = new Audio('./audio/ouch.mp3')
-ouch.volume = 0.1
+ouch.volume = 0.3
 const stinger = new Audio('./audio/stinger.wav');
 stinger.volume = 0.5
 const cutsceneAudio = new Audio('./audio/cutsceneaudio.wav')
@@ -267,6 +267,13 @@ function startGame() {
   enemiesDisplay.innerText = `Enemies: ${enemiesRemaining}`
 }
 function waveCleared() {
+  if (wave === 6) {
+    vamp.pause()
+    vamp.currentTime = 0
+    setTimeout(() => {
+      vamp.play()
+    }, 6000)
+  }
   play = false
   clearInterval(moveTime)
   clearInterval(powerSpawnTime)
